c4ac47826b34efcbba2d3d71aeeb3542b7bbea09
afraidofhash <abhinpal67@gmail.com>
2022-02-17 11:03:33 +0530

    Updates

---
+++ precache-manifest.63f62072c9915472b232829378b2e750.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f657921bf20a1c29c806e1c109d7566b",
+    "url": "/backend/index.html"
+  },
+  {
+    "revision": "a78755e5ca783449372e",
+    "url": "/backend/static/css/2.57ec547b.chunk.css"
+  },
+  {
+    "revision": "211067dd1ccf8dd1e614",
+    "url": "/backend/static/css/main.13c6f325.chunk.css"
+  },
+  {
+    "revision": "a78755e5ca783449372e",
+    "url": "/backend/static/js/2.394708ab.chunk.js"
+  },
+  {
+    "revision": "fafe79a98b490e121e6e9e13cc505574",
+    "url": "/backend/static/js/2.394708ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211067dd1ccf8dd1e614",
+    "url": "/backend/static/js/main.7ccb3b6b.chunk.js"
+  },
+  {
+    "revision": "5b90503c178ec9cff68d",
+    "url": "/backend/static/js/runtime-main.ea34169d.js"
+  },
+  {
+    "revision": "0723ff378addd6103ff5d3abf9b09172",
+    "url": "/backend/static/media/hero-background.0723ff37.jpg"
+  }
+]);
